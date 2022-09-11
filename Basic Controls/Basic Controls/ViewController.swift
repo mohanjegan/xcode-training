ac72d836024dbcb394a00ddb0d6a390601c428a8
Mohan <108399135+mohanjegan@users.noreply.github.com>
2022-09-11 15:34:11 +0530

    basic controls

---
+++ Basic Controls/Basic Controls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic Controls
+//
+//  Created by Mohan on 11/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
