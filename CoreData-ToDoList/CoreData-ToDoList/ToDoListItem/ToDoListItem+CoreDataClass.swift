89989af16eadd42eabf9829407099c7227c8be97
Mohan <108399135+mohanjegan@users.noreply.github.com>
2022-09-12 14:38:48 +0530

    CoreData ToDoList
    using tableview, dynamic cells, deque reuasble cell, coredata to store, navigation controller to have title and nav bar button.

---
+++ CoreData-ToDoList/CoreData-ToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreData-TodoList
+//
+//  Created by Mohan on 12/09/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ CoreData-ToDoList/CoreData-ToDoList/ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  CoreData-TodoList
+//
+//  Created by Mohan on 12/09/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
