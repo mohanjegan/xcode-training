a0a16e2cd2be30e99dcf04db0f1adaa92a83cbfb
Mohan <108399135+mohanjegan@users.noreply.github.com>
2022-09-15 11:34:45 +0530

    Image Picker Controller
    select image from photo gallery using image picker controller

---
+++ ImagePickerFromUser/ImagePickerFromUser/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ImagePickerFromUser
+//
+//  Created by Mohan on 15/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func didTapButton(){
+        
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage{
+            imageView.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
