65cdebeef7ad40f02c67a0a19f18076bb4ef7288
Mohan Pradeep <108399135+mohanjegan@users.noreply.github.com>
2022-09-26 18:48:53 +0530

    Json data in collectionView

---
+++ Json-in-collectionview/Json-in-collectionview/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  Json-in-collectionview
+//
+//  Created by Mohan on 26/09/22.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+}
